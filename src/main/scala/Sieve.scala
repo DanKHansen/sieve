e2aa52dda501ba67be302c23adf79b981176d8c7
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-16 11:38:23 +0200

    solution

--- src/main/scala/Sieve.scala
+++ src/main/scala/Sieve.scala
@@ -1,2 +1,8 @@
 object Sieve:
-   def primes(n: Int): List[Int] = ???
+   def primes(n: Int): Seq[Int] =
+      def isPrime(i: Int): Boolean =
+         if i <= 1 then false
+         else if i == 2 then true
+         else !(2 until i).exists(n => i % n == 0)
+      (1 to n).filter(isPrime)   
+         
