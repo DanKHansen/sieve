402e10eb9c1c16ce2a37bf1251a1c9a5d1761ae8
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-16 11:43:50 +0200

    refactor

--- src/main/scala/Sieve.scala
+++ src/main/scala/Sieve.scala
@@ -1,8 +1,2 @@
 object Sieve:
-   def primes(n: Int): Seq[Int] =
-      def isPrime(i: Int): Boolean =
-         if i <= 1 then false
-         else if i == 2 then true
-         else !(2 until i).exists(n => i % n == 0)
-      (1 to n).filter(isPrime)   
-         
+   def primes(n: Int): Seq[Int] = (2 to n).filterNot(i => (2 until i).exists(j => i % j == 0))
